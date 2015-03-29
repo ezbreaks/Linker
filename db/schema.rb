22da2b8810489a777d6027878dc59fd214f890e9
Chirvase Dragos Andrei <chirvase.dragos.andrei@gmail.com>
2015-03-29 02:44:17 +0200

    comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328213131) do
+ActiveRecord::Schema.define(version: 20150329000944) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "commnets", force: :cascade do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "commnets", ["link_id"], name: "index_commnets_on_link_id"
+  add_index "commnets", ["user_id"], name: "index_commnets_on_user_id"
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
